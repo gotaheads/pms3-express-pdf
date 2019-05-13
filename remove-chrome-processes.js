1d484c271d0dfe4f684bce501ee1e77c4fe4f883
gotaheads <gotahiroki@gmail.com>
2019-05-14 05:13:45 +1000

    clean up logging

--- remove-chrome-processes.js
+++ remove-chrome-processes.js
@@ -3,15 +3,15 @@ const R = require('ramda');
 const {equals} = R;
 const output = shell.exec('ps -eo pid,etime,comm |grep chrome', {silent: true}).stdout;
 const lines = output.split('chrome');
-console.log('lines: %j', output, lines);
+console.log('output: %s, lines: %j', output, lines);
 const processesToKill = lines
   .filter(line => {
     return equals((line.trim().match(/\S+/g) || []).length, 2);
   })
   .map(line => {
     const parts = line.trim().match(/\S+/g) || [];
-    //console.log('parts: %j', parts);
-    return {id: +parts[0].trim(), time: +parts[1].trim().replace(/:/, '')};
+    console.log('parts: %j', parts);
+    return {id: +parts[0].trim(), time: +parts[1].trim().replace(/:/g, '')};
   })
   .filter(process => process.time > 1000);
 
@@ -21,5 +21,3 @@ processesToKill.forEach(proc => {
   const output = shell.exec(`kill -15  ${proc.id}`).stdout;
   //console.log('killing the prcess: %j, output: %j', proc, output);
 })
-
-
